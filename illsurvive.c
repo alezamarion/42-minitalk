1dda8692ecf68c1c7bdca967897428f529d17a39
alezamarion <alezamarion@icloud.com>
2021-10-19 21:29:05 -0300

    other studies

--- illsurvive.c
+++ illsurvive.c
@@ -4,6 +4,7 @@
 // SIGHUP: sent to a process when its controlling terminal is closed
 // SIGTERM: sent to a process to request its termination
 // ps aux | grep illsurvive | grep -v grep -> to get current PID 
+// SIGKILL and SIGSTOP cannot be changed
 
 
 #include <signal.h>
@@ -14,22 +15,27 @@
 //handler for common signal that terminate process
 static void end_handler(int signal)
 {
-    printf("I'll survive, baby");
+    printf("I'll survive, baby\n");
 }
 
 int main(int argc, char *argv[])
 {
-    printf("PID = %d", getpid());
+    printf("PID = %d\n", getpid());
+    int a = 0;
     //set handlers
-    if ((signal(SIGINT, end_handler) == SIG_ERR) || (signal(SIGHUP, end_handler) == SIG_ERR) || (signal(SIGTERM, end_handler) == SIG_ERR))
+    if ((signal(SIGINT, end_handler) == SIG_ERR) || (signal(SIGHUP, end_handler) == SIG_ERR)
+        || (signal(SIGTERM, end_handler) == SIG_ERR))
     {
         printf("Error while setting a signal handler\n");
         exit(EXIT_FAILURE);
     }
     //infinite loop
-    while(1) 
+    while (1)
     {
-
+        printf("a = %d\n", a);
+        a++;
+        sleep(2);
     }
     return (0); 
+
 }
