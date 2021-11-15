5ccf46a5882692dcba80107320a90551454d97d2
alezamarion <alezamarion@icloud.com>
2021-11-15 15:20:57 +0100

    submitted with 125% + comments

--- src/client.c
+++ src/client.c
@@ -6,24 +6,9 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 22:55:23 by azamario          #+#    #+#             */
-/*   Updated: 2021/11/11 20:17:26 by azamario         ###   ########.fr       */
+/*   Updated: 2021/11/15 15:17:25 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* 
-1 - o cliente recebe dois parametros:
-    - o PID do server
-    - a string que sera enviada
-
-2 - o cliente tem que passar a string como parametro para o server 
-3 - assim que o server receber a string, ele deve imprimi-la
-4 - a comunicacao entre os programas sera feira entre sinais UNIX
-5 - o server tem que ser capaz de imprimir a string rapidamente
-6 - o server deve ser capaz de receber strings de varios clientes, sem precisar ser reinicializado
-7 - apenas dois sinais sao permitidos: SIGUSR1 e SIGUSR2
-
-cliente manda para o server
-*/
 
 #include <signal.h>
 #include "libft.h"
@@ -37,18 +22,37 @@ void	wait_server_response(void)
 	g_done = 0;
 }
 
+/*
+	4. When the Server sends the SIGUSR1 signal indicating that it received the bit sent by the Client,
+	the variable value g_done is changed to 1 and the wait_server_response function ends the wait loop.
+*/
+
+void	sig_handler(int signal)
+{
+	g_done = 1;
+	(void)signal;
+}
+
+/*
+	3. This function will decompose the string into bits using bitwise operators and send it to the 
+	Server, using signals SIGUSR1 representing bit 1 and SIGUSR2 representing bit 0.
+	Counter variable is initialized using bitwise left shift and incremented using bitwise right shift.
+	A bitwise & operation is used to get the bit that will be sent to the Server.
+	For each bit sent, an acknowledgment of receipt by the Server is expected.
+*/
+
 void	send_signal(int pid, const char *str)
 {
 	int	count;
-	int	bit;
+	int	bit;								
 
-	count = 1 << 7;							//00000001 << 7 == 10000000
+	count = 1 << 7;
 	while (count)
-	{										// string "A" == 01000001
-		bit = *str & count;					// 1a passagem: 01000001 & 10000000 == 00000000 envia 0	
-		if (bit)							// 2a passagem: 01000001 & 01000000 == 01000000 envia 1
+	{
+		bit = *str & count;
+		if (bit)
 		{
-			if (kill(pid, SIGUSR1))			//pid server - dispara sighandler do server
+			if (kill(pid, SIGUSR1))
 				exit(EXIT_FAILURE);
 		}
 		else
@@ -56,22 +60,28 @@ void	send_signal(int pid, const char *str)
 			if (kill(pid, SIGUSR2))
 				exit(EXIT_FAILURE);
 		}
-		wait_server_response();				//aguarda resposta server
-		count >>= 1;						//após a primeira passagem == 01000000
+		wait_server_response();
+		count >>= 1;
 	}
 }
+
+/*
+	2. This function receives the string that will be processed through the send_signal function. 
+	When the string ends, 8 bits 0 are sent to the Server. For each bit sent, a confirmation of receipt -
+	by Server - is awaited.
+*/
 
 void	process_str(int pid, const char *str)
 {
 	int	count;
 
-	while (*str)								//transforma a string em 0 e 1 e envia para o server
+	while (*str)
 	{
 		send_signal(pid, str);
 		str++;
 	}		
 	count = 8;								
-	while (count--)								//envia byte nulo quando acaba a string
+	while (count--)
 	{
 		if (kill(pid, SIGUSR2))
 			exit(EXIT_FAILURE);
@@ -79,11 +89,11 @@ void	process_str(int pid, const char *str)
 	}
 }
 
-void	sig_handler(int signal) 				//é disparado pelo kill do server
-{
-	g_done = 1;
-	(void)signal;
-}
+/*
+	1. Here the sigaction function is used to handle the SIGUSR1 signal received by the Server. 
+	This signal will be used as confirmation of receiving - by the Server - the signals sent by the Client.
+	The process_str function receives the Server PID and the string that will be sent to the Server.
+*/
 
 int	main(int argc, char const *argv[])
 {
@@ -94,7 +104,7 @@ int	main(int argc, char const *argv[])
 		exit(EXIT_FAILURE);
 	ft_bzero(&action, sizeof(struct sigaction));
 	action.sa_handler = sig_handler;
-	if (sigaction(SIGUSR1, &action, NULL))		//server só envia o sigusr1 para o client para confirmar que recebeu sinal
+	if (sigaction(SIGUSR1, &action, NULL))
 		exit(EXIT_FAILURE);
 	pid = ft_atoi(argv[1]);
 	process_str(pid, argv[2]);
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 22:54:56 by azamario          #+#    #+#             */
-/*   Updated: 2021/11/11 22:07:45 by azamario         ###   ########.fr       */
+/*   Updated: 2021/11/15 14:49:47 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,29 @@ char	*print_string(char *str)
 	return (NULL);
 }
 
-void	sig_handler(int signal, siginfo_t *siginfo, void *context) // siginfo pega automaticamente o pid do sinal recebido
+/*
+	3. This function is called every time Server receives a signal (SIGUSR1 or SIGUSR2) from Client.
+	SIGUSR1 represents 1; SIGUSR2 represents 0. 
+	- The string is decomposed into bits by the Client using bitwise operations and sent via signals here
+	to the Server. 
+	- It is then processed again via bitwise. Every 8 bits processed, they are stored in the static 
+	  variable message using function ft_strappendchar.
+	- For each bit received, a signal is sent to the Client. When the string finishes being processed
+	  by the Client, 8 bits 0 are sent to the Server and then the message is printed by the Server.
+*/
+
+void	sig_handler(int signal, siginfo_t *siginfo, void *context)
 {
 	static char	current;
 	static int	bits;
-	static char	*message;			//recebe char 'A': 01000001
+	static char	*message;
 
 	if (current == 0)
-		current = 0xFF;				//current == 11111111
-	if (signal == SIGUSR1)			//segunda passagem:  01111111 | 10000000 >> 1 == 01111111 | 01000000 == 01111111
+		current = 0xFF;
+	if (signal == SIGUSR1)
 		current |= 0x80 >> bits;
-	else if (signal == SIGUSR2)		//primeira passagem: 11111111 ˆ 10000000 >> 0 == 11111111 ^ 10000000 == 01111111
-		current ^= 0x80 >> bits;	//terceita passagem: 01111111 ^ 10000000 >> 2 == 01111111 ^ 00100000 == 01011111
+	else if (signal == SIGUSR2)
+		current ^= 0x80 >> bits;
 	if (++bits == 8)
 	{
 		if (current)
@@ -45,24 +56,33 @@ void	sig_handler(int signal, siginfo_t *siginfo, void *context) // siginfo pega 
 		current = 0xFF;
 		bits = 0;
 	}
-	kill(siginfo->si_pid, SIGUSR1); 				//si_pid = pid do client, dispara o sighandler do client, mandando um SIGUSR1
+	kill(siginfo->si_pid, SIGUSR1);
 	(void)context;
 }
+
+/*
+	2. We will process two signals received from the Client: SIGUSR1 and SIGUSR2
+*/
 
 int	initialize_server(void)
 {
 	struct sigaction	action;
 
 	ft_bzero(&action, sizeof(struct sigaction));
-	sigemptyset(&action.sa_mask);					//initialize and empty a signal set
+	sigemptyset(&action.sa_mask);
 	action.sa_sigaction = sig_handler;
-	action.sa_flags = SA_SIGINFO;					// para utilizar a sa_sigaction				
-	if (sigaction(SIGUSR1, &action, NULL))			//retorna 0 se sucesso
+	action.sa_flags = SA_SIGINFO;			
+	if (sigaction(SIGUSR1, &action, NULL))
 		exit(EXIT_FAILURE);
 	if (sigaction(SIGUSR2, &action, NULL))
 		exit(EXIT_FAILURE);
 	return (0);
 }
+
+/*
+	1. Here we get and print the Server PID and initialize the settings of the sigaction function that will be used
+	to process the signals received by the Client	
+*/
 
 int	main(void)
 {
@@ -74,6 +94,6 @@ int	main(void)
 	ft_putstr_fd("\n", 1);
 	initialize_server();
 	while (1)
-		pause();									//sleep until a signal is delivered that either
-	return (0);										//terminates the process or causes the invocation 
-}													//of a signal handler			
+		pause();
+	return (0); 
+}		
